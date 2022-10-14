478863b82ca9d4adb2aea9663600b5e86024b975
Shawn Davenport <shawn.davenport@quarkworks.co>
2022-10-14 13:34:40 -0500

    Improved efficiency

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,12 @@
-for x in range(100):
+for x in range(1, 101):
     output = ''
-    num = x + 1
-    if num % 3 == 0:
-        if num % 5 == 0:
+    if x % 3 == 0:
+        if x % 5 == 0:
             output = 'FizzBuzz'
         else:
             output = 'Fizz'
-    elif num % 5 == 0:
+    elif x % 5 == 0:
         output = 'Buzz'
     else:
-        output = num
+        output = x
     print(output)
